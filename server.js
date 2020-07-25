9aa17ad984fc9fc1e0359093f822e183ad2149d8
Kuddleman <edl1155@gmail.com>
2020-07-25 12:47:28 -0700

    add npm package morgan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const logger = require('./middleware/logger')
+const morgan = require('morgan')
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
@@ -9,11 +10,13 @@ const bootcamps = require('./routes/bootcamps')
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+if ( process.env.NODE_ENV === 'development' ) {
+  app.use( morgan( 'dev' ) )
+}
 
 
+// Dev logging middleware.  Run only in dev environment
 
-//USE middleware:
-app.use( logger )
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
